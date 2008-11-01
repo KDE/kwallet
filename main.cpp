8fe6db1538029fa927b264041a2d1d72a271f986
Michael Leupold <lemma@confuego.org>
2008-11-01 20:55:27 +0000

    Finally add the authors to the KAboutData.
    svn path=/trunk/KDE/kdebase/runtime/; revision=878813

--- main.cpp
+++ main.cpp
@@ -38,10 +38,9 @@ extern "C" KDE_EXPORT int kdemain(int argc, char **argv)
     KAboutData aboutdata("kwalletd", 0, ki18n("KDE Wallet Service"),
                          "0.1", ki18n("KDE Wallet Service"),
                          KAboutData::License_LGPL, ki18n("(C) 2002-2008 George Staikos, Michael Leupold, Thiago Maceira"));
-    // STRING FREEZE!
-    // aboutdata.addAuthor(ki18n("Michael Leupold"),ki18n("Maintainer"),"lemma@confuego.org");
-    // aboutdata.addAuthor(ki18n("George Staikos"),ki18n("Former maintainer"),"staikos@kde.org");
-    // aboutdata.addAuthor(ki18n("Thiago Maceira"),ki18n("DBus Interface"),"thiago@kde.org");
+    aboutdata.addAuthor(ki18n("Michael Leupold"),ki18n("Maintainer"),"lemma@confuego.org");
+    aboutdata.addAuthor(ki18n("George Staikos"),ki18n("Former maintainer"),"staikos@kde.org");
+    aboutdata.addAuthor(ki18n("Thiago Maceira"),ki18n("DBus Interface"),"thiago@kde.org");
 
     KCmdLineArgs::init( argc, argv, &aboutdata );
     KUniqueApplication::addCmdLineOptions();
