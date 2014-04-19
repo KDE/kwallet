7035a9f45ee74481fad114a26a964c356aec732a
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-04-19 20:38:12 -0300

    Rewrite FindLibGcrypt.cmake to not use gcrypt-config.
    Using the gcrypt-config shell script won't work on Windows.
    I wrote a new find module from scratch. It now searches for the
    library and include directory the usual CMake way (find_library and
    find_path). It still supports version checking by extracting the
    version number from the gcrypt.h header.
    
    REVIEW:117652

--- cmake/FindLibGcrypt.cmake
+++ cmake/FindLibGcrypt.cmake
@@ -1,61 +1,82 @@
-
-# - Try to find the Gcrypt library
-# Once run this will define
+#.rst
+# FindLibGcrypt
+# -------------
 #
-#  LIBGCRYPT_FOUND - set if the system has the gcrypt library
-#  LIBGCRYPT_CFLAGS - the required gcrypt compilation flags
-#  LIBGCRYPT_LIBRARIES - the linker libraries needed to use the gcrypt library
+# Finds the Libgcrypt library.
 #
-# Copyright (c) 2006 Brad Hards <bradh@kde.org>
+# This will define the following variables:
 #
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+# ``LIBGCRYPT_FOUND``
+#     True if the requested version of gcrypt was found
+# ``LIBGCRYPT_VERSION``
+#     The version of gcrypt that was found
+# ``LIBGCRYPT_INCLUDE_DIRS``
+#     The gcrypt include directories
+# ``LIBGCRYPT_LIBRARIES``
+#     The linker libraries needed to use the gcrypt library
 
-# libgcrypt is moving to pkg-config, but earlier version don't have it
+# Copyright 2014 Nicolás Alvarez <nicolas.alvarez@gmail.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-if(NOT LibGcrypt_FIND_VERSION)
-    set(LibGcrypt_FIND_VERSION "1.5.0")
+find_program(LIBGCRYPTCONFIG_SCRIPT NAMES libgcrypt-config)
+if(LIBGCRYPTCONFIG_SCRIPT)
+    execute_process(COMMAND "${LIBGCRYPTCONFIG_SCRIPT}" --prefix OUTPUT_VARIABLE PREFIX)
+    set(LIBGCRYPT_LIB_HINT "${PREFIX}/lib")
+    set(LIBGCRYPT_INCLUDE_HINT "${PREFIX}/include")
 endif()
 
-#search in typical paths for libgcrypt-config
-FIND_PROGRAM(LIBGCRYPTCONFIG_EXECUTABLE NAMES libgcrypt-config)
+find_library(LIBGCRYPT_LIBRARY
+    NAMES gcrypt
+    HINTS ${LIBGCRYPT_LIB_HINT}
+)
+find_path(LIBGCRYPT_INCLUDE_DIR
+    NAMES gcrypt.h
+    HINTS ${LIBGCRYPT_INCLUDE_HINT}
+)
 
-#reset variables
-set(LIBGCRYPT_LIBRARIES)
-set(LIBGCRYPT_CFLAGS)
+if(LIBGCRYPT_INCLUDE_DIR)
+    file(STRINGS ${LIBGCRYPT_INCLUDE_DIR}/gcrypt.h GCRYPT_H REGEX "^#define GCRYPT_VERSION ")
+    string(REGEX REPLACE "^#define GCRYPT_VERSION \"(.*)\".*$" "\\1" LIBGCRYPT_VERSION "${GCRYPT_H}")
+endif()
 
-# if libgcrypt-config has been found
-IF(LIBGCRYPTCONFIG_EXECUTABLE)
+include(FindPackageHandleStandardArgs)
 
-  # workaround for MinGW/MSYS
-  # CMake can't starts shell scripts on windows so it need to use sh.exe
-  EXECUTE_PROCESS(COMMAND sh ${LIBGCRYPTCONFIG_EXECUTABLE} --libs RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE LIBGCRYPT_LIBRARIES OUTPUT_STRIP_TRAILING_WHITESPACE)
-  EXECUTE_PROCESS(COMMAND sh ${LIBGCRYPTCONFIG_EXECUTABLE} --cflags RESULT_VARIABLE _return_VALUE OUTPUT_VARIABLE LIBGCRYPT_CFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
-  EXECUTE_PROCESS(COMMAND sh ${LIBGCRYPTCONFIG_EXECUTABLE} --version RESULT_VARIABLE _return_VALUEVersion OUTPUT_VARIABLE LIBGCRYPT_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+find_package_handle_standard_args(LibGcrypt
+    FOUND_VAR LIBGCRYPT_FOUND
+    REQUIRED_VARS LIBGCRYPT_LIBRARY LIBGCRYPT_INCLUDE_DIR
+    VERSION_VAR LIBGCRYPT_VERSION
+)
+if(LIBGCRYPT_FOUND)
+    set(LIBGCRYPT_LIBRARIES ${LIBGCRYPT_LIBRARY})
+    set(LIBGCRYPT_INCLUDE_DIRS ${LIBGCRYPT_INCLUDE_DIR})
+endif()
 
-  IF(NOT LIBGCRYPT_CFLAGS AND NOT _return_VALUE)
-    SET(LIBGCRYPT_CFLAGS " ")
-  ENDIF(NOT LIBGCRYPT_CFLAGS AND NOT _return_VALUE)
+mark_as_advanced(LIBGCRYPT_LIBRARY LIBGCRYPT_INCLUDE_DIR)
 
-  IF(LIBGCRYPT_LIBRARIES AND LIBGCRYPT_CFLAGS)
-    SET(LIBGCRYPT_FOUND TRUE)
-  ENDIF(LIBGCRYPT_LIBRARIES AND LIBGCRYPT_CFLAGS)
-
-  if(LIBGCRYPT_VERSION VERSION_LESS ${LibGcrypt_FIND_VERSION})
-     message(WARNING "libgcrypt found but version is less than required, Found ${LIBGCRYPT_VERSION} Required ${LibGcrypt_FIND_VERSION}")
-     SET(LIBGCRYPT_FOUND FALSE)
-  endif()
-
-ENDIF(LIBGCRYPTCONFIG_EXECUTABLE)
-
-if (LIBGCRYPT_FOUND)
-   if (NOT LibGcrypt_FIND_QUIETLY)
-      message(STATUS "Found libgcrypt: ${LIBGCRYPT_LIBRARIES}")
-   endif (NOT LibGcrypt_FIND_QUIETLY)
-else (LIBGCRYPT_FOUND)
-   if (LibGcrypt_FIND_REQUIRED)
-      message(WARNING "Could not find libgcrypt libraries")
-   endif (LibGcrypt_FIND_REQUIRED)
-endif (LIBGCRYPT_FOUND)
-
-MARK_AS_ADVANCED(LIBGCRYPT_CFLAGS LIBGCRYPT_LIBRARIES)
+include(FeatureSummary)
+set_package_properties(LibGcrypt PROPERTIES
+    DESCRIPTION "A general purpose cryptographic library based on the code from GnuPG."
+    URL "http://www.gnu.org/software/libgcrypt/"
+)
