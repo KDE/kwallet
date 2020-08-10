6462a948c14393f49bd350856c26f7cdf72c6e95
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-08-10 15:12:29 +0000

    KWallet: Convert copyright statements to SPDX expressions

--- src/runtime/kwalletd/backend/sha1.cc
+++ src/runtime/kwalletd/backend/sha1.cc
@@ -1,21 +1,11 @@
-/* This file is part of the KDE project
-   Copyright (C) 2001 George Staikos <staikos@kde.org>
-   Based heavily on SHA1 code from GPG 1.0.3 (C) 1998 FSF
+/*
+    This file is part of the KDE project
+    SPDX-FileCopyrightText: 2001 George Staikos <staikos@kde.org>
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
+    Based heavily on SHA1 code from GPG 1.0.3:
+    SPDX-FileCopyrightText: 1998 FSF
 
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
+    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
 #include "sha1.h"
--- src/runtime/kwalletd/kwalletd.h
+++ src/runtime/kwalletd/kwalletd.h
@@ -1,25 +1,11 @@
 /*
-   This file is part of the KDE libraries
+    This file is part of the KDE libraries
+    SPDX-FileCopyrightText: 2002-2004 George Staikos <staikos@kde.org>
+    SPDX-FileCopyrightText: 2008 Michael Leupold <lemma@confuego.org>
 
-   Copyright (c) 2002-2004 George Staikos <staikos@kde.org>
-   Copyright (c) 2008 Michael Leupold <lemma@confuego.org>
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
-
+    SPDX-License-Identifier: LGPL-2.0-or-later
 */
+
 #ifndef _KWALLETD_H_
 #define _KWALLETD_H_
 
