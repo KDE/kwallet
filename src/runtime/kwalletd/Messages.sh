2e13cbc130ba0b22b6307cfc2036703c29c1794f
Luigi Toscano <luigi.toscano@tiscali.it>
2023-03-20 19:35:07 +0100

    Fix the translation template version number (5->6)
    KF5 and KF6 must be co-installable, so change the major version
    number which is part of the translation template name.

--- src/runtime/kwalletd/Messages.sh
+++ src/runtime/kwalletd/Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp" -o -name "*.cc" | grep -v "/tests"` -o $podir/kwalletd5.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.cc" | grep -v "/tests"` -o $podir/kwalletd6.pot
