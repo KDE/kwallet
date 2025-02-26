66bd5f35470a379e62ee972c3140f3dc7e9fa88c
Marco Martin <notmart@gmail.com>
2025-02-26 15:46:12 +0100

    Compatibility for binary data
    correctly read existing binary data saved as octet stream,
    but re-write it as base64 encoded to ensure future compatibility with
    QtKeychain

--- src/runtime/kwalletd/secretserviceclient.h
+++ src/runtime/kwalletd/secretserviceclient.h
@@ -38,8 +38,10 @@ class SecretServiceClient : public QObject
 public:
     enum Type {
         PlainText = 0,
+        Base64,
         Binary,
-        Map
+        Map,
+        Unknown
     };
     Q_ENUM(Type);
 
@@ -92,7 +94,7 @@ protected:
     void onServiceOwnerChanged(const QString &serviceName, const QString &oldOwner, const QString &newOwner);
 
     SecretCollection *retrieveCollection(const QString &name);
-    SecretItemPtr retrieveItem(const QString &key, const QString &folder, const QString &collectionName, bool *ok);
+    SecretItemPtr retrieveItem(const QString &key, const SecretServiceClient::Type type, const QString &folder, const QString &collectionName, bool *ok);
 
 protected Q_SLOTS:
     void handlePrompt(bool dismissed);
